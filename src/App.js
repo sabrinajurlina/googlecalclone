8c4957e16cab670c89a1c091ee6132175f8cd983
Sabrina Jurlina <sabrina.jurlina@gmail.com>
2022-08-19 08:23:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createRef } from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+import resources from './fixtures/resources.json'
+import events from './fixtures/events.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class DemoApp extends React.Component {
+  render() {
+
+  const calendarRef = createRef()
+
+    return (
+     
+      <FullCalendar
+        ref={calendarRef}
+        schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
+        plugins={[ dayGridPlugin, timeGridPlugin, resourceTimelinePlugin ]}
+        initialView="dayGridMonth"
+        events="https://fullcalendar.io/demo-events.json?single-day&with-nesting&for-resource-timeline"
+        resources="https://fullcalendar.io/demo-resources.json?with-nesting&with-colors"
+        customButtons={{
+          myTimeDayBtn: {
+            text: "timeDay",
+            click() {
+              const calendar = calendarRef.current;
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+                calendarApi.changeView('timeGridDay')
+              }
+            },
+          },
+          myTimeWeekBtn: {
+            text: "timeWeek",
+            click() {
+              const calendar = calendarRef.current;
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+                calendarApi.changeView('timeGridWeek')
+              }
+            },
+          },
+          myTimelineDayBtn: {
+            text: "timelineDay",
+            click() {
+              const calendar = calendarRef.current;
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+                calendarApi.changeView('resourceTimelineDay')
+              }
+            },
+          },
+          myTimelineWeekBtn: {
+            text: "timelineWeek",
+            click() {
+              const calendar = calendarRef.current;
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+                calendarApi.changeView('resourceTimelineWeek')
+              }
+            },
+          },
+        }}
+        headerToolbar={{
+          left: "prev,next",
+          center: "title",
+          right: "today,dayGridDay,dayGridWeek,dayGridMonth,myTimeDayBtn,myTimeWeekBtn,myTimelineDayBtn,myTimelineWeekBtn",
+        }}
+      />
+   
+  )
+}}
